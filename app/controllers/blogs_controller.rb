c8690208b858c9230be94faf2ff422026ab3d4d2
isamu_yonekawa <8orange.pp@gmail.com>
2018-04-03 16:22:32 +0900

    Design editing

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,10 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @blogs = Blog.all.order(created_at: :desc)
   end
-  
+
   def new
     if params[:back]
       @blog = Blog.new(blog_params)
@@ -12,7 +12,7 @@ class BlogsController < ApplicationController
       @blog = Blog.new
     end
   end
-  
+
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
@@ -21,15 +21,15 @@ class BlogsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
-    
+
   end
-  
+
   def edit
-    
+
   end
-  
+
   def update
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: 'ブログを編集しました！'
@@ -37,22 +37,28 @@ class BlogsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
   end
-  
+
   def confirm
     @blog = Blog.new(blog_params)
     render :new if @blog.invalid?
   end
-  
+
+  def top
+  end
+
+
+
+
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
-  
+
   def set_blog
     @blog = Blog.find(params[:id])
   end
