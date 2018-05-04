1cedcaec2187fc31e4f74319c0569decd99493d3
isamu_yonekawa <8orange.pp@gmail.com>
2018-05-04 22:36:30 +0900

    redirect_toの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      flash[:notice] = '登録が完了しました。ログインしてください。'
+      redirect_to new_session_path
     else
       render 'new'
     end
